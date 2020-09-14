9bc1babbdd3f179d0220c3aba0f1b0074b479ca6
Thad House <thhous@microsoft.com>
2020-09-14 11:30:24 -0700

    Lower per proc limit even more

--- src/platform/datapath_winkernel.c
+++ src/platform/datapath_winkernel.c
@@ -92,7 +92,7 @@ typedef enum {
 //
 // The maximum allowed pending WSK buffers per proc before copying.
 //
-#define PENDING_BUFFER_LIMIT                256000
+#define PENDING_BUFFER_LIMIT                32000
 
 static_assert(
     sizeof(QUIC_BUFFER) == sizeof(WSABUF),
